e8fa11be594f9cce923b3662d82d67cbeffd5633
Heavu1 <81611146+Heavu1@users.noreply.github.com>
2021-07-29 23:07:31 +0800

    Create redostk.java

---
+++ redostk.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2021 tu.cn All right reserved. This software is the
+ * confidential and proprietary information of tu.cn ("Confidential
+ * Information"). You shall not disclose such Confidential Information and shall
+ * use it only in accordance with the terms of the license agreement you entered
+ * into with Tu.cn
+ */
+
+import java.util.Stack;
+
+/**
+ * @author Zhenghy
+ * @date 2021/7/29 17:31
+ */
+interface redostk {
+    Stack<Object> redo_iconstk1 = new Stack<>();
+    Stack<Object> redo_iconstk2 = new Stack<>();
+    Stack<Object> redo_posstk1 = new Stack<>();
+    Stack<Object> redo_posstk2 = new Stack<>();
+}
